c353db9566f0e1107ee94d33296c8b9a65bd2235
RobVermeer <rob@vermeertech.nl>
2023-08-19 13:16:03 +0200

    Change submit button icon

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -1,8 +1,13 @@
 import { experimental_useFormStatus as useFormStatus } from "react-dom"
 import { Button } from "../ui/button"
+import { Plus } from "lucide-react"
 
 export const AddTodo = () => {
   const { pending } = useFormStatus()
 
-  return <Button disabled={pending}>Submit</Button>
+  return (
+    <Button className="shrink-0" disabled={pending} size="icon">
+      <Plus size="20" />
+    </Button>
+  )
 }
