ac5bb92119f00957df0cf276f4906851ff8190f0
RobVermeer <rob@vermeertech.nl>
2023-08-19 22:14:01 +0200

    Change font to Ubuntu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Ubuntu } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] })
+const ubuntu = Ubuntu({ weight: ["400"], subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "36 hours",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ubuntu.className}>{children}</body>
     </html>
   )
 }
