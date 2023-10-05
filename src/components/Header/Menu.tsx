1bbb38c92275f2a29f3f47fa432084b8e297d6b5
RobVermeer <rob@vermeertech.nl>
2023-10-05 21:28:45 +0200

    Use sheet for header menu

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -2,77 +2,104 @@
 
 import { Session } from "next-auth"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu"
 import { signOut } from "next-auth/react"
 import { CheckCheck, FileClock, ListTodo, LogOut, TimerOff } from "lucide-react"
 import Link from "next/link"
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import { Logo } from "@/components/Logo"
+import { useState } from "react"
 
 interface Props {
   session: Session
 }
 
 export function Menu({ session }: Props) {
+  const [open, setOpen] = useState(false)
   const initials = session.user.name
     .split(" ")
     .map((part: string) => part.at(0))
     .slice(0, 2)
     .join("")
 
+  const close = () => setOpen(false)
+  const logout = () => {
+    signOut()
+    close()
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
         <Avatar className="border border-slate-500">
           <AvatarImage src={session.user.image} alt={`@${session.user.name}`} />
           <AvatarFallback>{initials}</AvatarFallback>
         </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 mr-2">
-        <DropdownMenuItem className="flex gap-2 align-center" asChild>
-          <Link href="/" scroll={false}>
-            <ListTodo size="16" /> Todo
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="flex gap-2 align-center" asChild>
-          <Link href="/completed" scroll={false}>
-            <CheckCheck
-              className="text-green-600 dark:text-green-400"
-              size="16"
-            />{" "}
-            Completed
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="flex gap-2 align-center" asChild>
-          <Link href="/expired" scroll={false}>
-            <TimerOff
-              className="text-yellow-600 dark:text-yellow-400"
-              size="16"
-            />{" "}
-            Expired
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem className="flex gap-2 align-center" asChild>
-          <Link href="/backlog" scroll={false}>
-            <FileClock
-              className="text-slate-600 dark:text-slate-400"
-              size="16"
-            />{" "}
-            Backlog
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="flex gap-2 align-center"
-          onClick={() => signOut()}
+      </SheetTrigger>
+      <SheetContent side="top" className="grid gap-3">
+        <SheetHeader>
+          <SheetTitle>
+            <Logo
+              className="justify-center text-slate-900 dark:text-white"
+              onClick={close}
+            />
+          </SheetTitle>
+        </SheetHeader>
+
+        <Link
+          href="/"
+          scroll={false}
+          onClick={close}
+          className="flex gap-2 items-center mt-4"
         >
-          <LogOut className="text-red-600 dark:text-red-400" size="16" /> Logout
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <ListTodo size="16" /> Todo
+        </Link>
+
+        <Link
+          href="/backlog"
+          scroll={false}
+          onClick={close}
+          className="flex gap-2 items-center"
+        >
+          <FileClock className="text-slate-400" size="16" /> Backlog
+        </Link>
+
+        <Link
+          href="/completed"
+          scroll={false}
+          onClick={close}
+          className="flex gap-2 items-center"
+        >
+          <CheckCheck
+            className="text-green-600 dark:text-green-400"
+            size="16"
+          />{" "}
+          Completed
+        </Link>
+
+        <Link
+          href="/expired"
+          scroll={false}
+          onClick={close}
+          className="flex gap-2 items-center"
+        >
+          <TimerOff
+            className="text-yellow-600 dark:text-yellow-400"
+            size="16"
+          />{" "}
+          Expired
+        </Link>
+
+        <Button className="flex gap-2 items-center mt-5" onClick={logout}>
+          <LogOut className="text-red-500 dark:text-red-400" size="16" /> Logout
+        </Button>
+      </SheetContent>
+    </Sheet>
   )
 }
