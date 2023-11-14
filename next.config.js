0a8fbf84567df012d18ad878e03e79bdeeae5cf3
RobVermeer <rob@vermeertech.nl>
2023-11-14 22:32:10 +0100

    Add domain to allowedOrigins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["36.robvermeer.nl"],
+    },
+  },
 }
 
 module.exports = nextConfig
