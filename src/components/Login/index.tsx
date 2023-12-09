5c54c18846a66823af79c832fa3c1dcdfe6eada9
RobVermeer <rob@vermeertech.nl>
2023-12-09 23:48:36 +0100

    Fix login button color

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -7,10 +7,10 @@ import { Chrome, Github } from "lucide-react"
 export function Login() {
   return (
     <main className="p-4 grid gap-3 fixed left-0 bottom-0 right-0 md:bottom-auto md:top-1/2 md:max-w-lg md:w-full md:mx-auto">
-      <Button onClick={() => signIn("github")}>
+      <Button variant="secondary" onClick={() => signIn("github")}>
         <Github size="18" className="mr-2" /> Login with Github
       </Button>
-      <Button onClick={() => signIn("google")}>
+      <Button variant="secondary" onClick={() => signIn("google")}>
         <Chrome size="18" className="mr-2" /> Login with Google
       </Button>
     </main>
